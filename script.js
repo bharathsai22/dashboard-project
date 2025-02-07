26845ff9ac98147f93701e449028100d38cb5d39
bharathsai22 <bharathsaigoli@gmail.com>
2025-02-07 21:45:08 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,5 @@
+document.getElementById('search-button').addEventListener('click', function () {
+    const query = document.getElementById('search-input').value;
+    alert(`Searching for "${query}"`);
+  });
+  
